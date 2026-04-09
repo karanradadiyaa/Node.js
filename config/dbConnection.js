f4a5a356a2e65c6861b711b6278e73f2397fbd04
karanradadiyaa <karanradadiya9636@gmail.com>
2026-04-09 10:22:49 +0530

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // DB Connection
 const connectDB = async () => {
-    mongoose.connect("mongodb://localhost:27017/store")
+    mongoose.connect("mongodb+srv://karanradadiya1132_db_user:<db_password>@cluster0.u6xy8zp.mongodb.net/")
     .then(() => console.log("DB Connected"))
     .catch(err => console.log(err));
 }
